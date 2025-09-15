6772a43a14959e33c47467e7ff154fcd05cbfc42
wjdsy <angel05020312@gmail.com>
2025-09-15 16:26:33 +0900

    wjdsy

--- Basic constants and printing/FileName.cpp
+++ Basic constants and printing/FileName.cpp
@@ -3,10 +3,10 @@
 int main(void)
 {
     printf("=== Basic Constants ===\n");
-    printf("Name\t: %s\n", "Sungwook");
-    printf("Initial\t: %c\n", 'S');
-    printf("Age : %d\n", 28);
-    printf("Score\t: %f\n", 93.5);
-    printf("Count\t: %u\n", 3000000000U);
+    printf("Name\t: %s\n\n", "Sungwook");
+    printf("Initial\t: %c\n\n", 'S');
+    printf("Age : %d\n\n", 28);
+    printf("Score\t: %f\n\n", 93.5);
+    printf("Count\t: %u\n\n", 3000000000U);
     return 0;
 }
