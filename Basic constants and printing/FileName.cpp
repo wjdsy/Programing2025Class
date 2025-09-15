ca2d67f48f5746e8c4612a9e368f3b7ae4da1c2e
wjdsy <angel05020312@gmail.com>
2025-09-15 16:04:39 +0900

    wjdsy

---
+++ Basic constants and printing/FileName.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("=== Basic Constants ===\n");
+    printf("Name\t: %s\n", "Sungwook");
+    printf("Initial\t: %c\n", 'S');
+    printf("Age : %d\n", 28);
+    printf("Score\t: %f\n", 93.5);
+    printf("Count\t: %u\n", 3000000000U);
+    return 0;
+}
