f19c003002254e2a9f94fc2a0bd19ccacd3cbe69
wjdsy <angel05020312@gmail.com>
2025-09-22 16:45:22 +0900

    wjdsy

--- Basic constants and printing3/FileName.cpp
+++ Basic constants and printing3/FileName.cpp
@@ -20,12 +20,12 @@ int main(void)
 
 	printf("Title : %s\n", title);
 
-	printf("PI default \t : %f\n", PI_VALUE);
-	printf("PI 2 digits \t : %.2f\n", PI_VALUE);
+	printf("PI default\t: %f\n", PI_VALUE);
+	printf("PI 2 digits\t: %.2f\n", PI_VALUE);
 
-	printf("Width demo \t : [%8d]\n", widt_demo_num);
+	printf("Width demo\t: [%8d]\n", widt_demo_num);
 
-	printf("Char sample \t : %c\n", sample_char);
-	printf("Backslach \t : \\\n");
+	printf("Char sample\t: %c\n", sample_char);
+	printf("Backslach\t: \\\n");
 	return 0;
 }
