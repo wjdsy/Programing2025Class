237063b70a4f27a5d017c10221facc9a7ed037c2
wjdsy <angel05020312@gmail.com>
2025-10-15 09:27:37 +0900

    wjdsy

---
+++ chast/FileName.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int chest;
+    char size;
+
+
+    printf("가슴둘레를 입력하세요 (cm): ");
+    scanf_s("%d", &chest);
+
+
+    if (chest <= 90) {
+        size = 'S';
+    }
+    else if (chest <= 100) {
+        size = 'M';
+    }
+    else {
+        size = 'L';
+    }
+
+    // 결과 출력
+    printf("가슴둘레: %dcm => 사이즈: %c\n", chest, size);
+
+    return 0;
+}
