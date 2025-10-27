881fb627df55edc30b54def450d3428e24dc1d07
wjdsy <angel05020312@gmail.com>
2025-10-27 17:15:48 +0900

    wjdsy

--- 18cm/FileName.cpp
+++ 18cm/FileName.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-// 함수 선언: 센티미터를 미터로 변환
+
 double centi_to_meter(double cm);
 
 int main(void)
 {
     double res;
 
-    res = centi_to_meter(187); 
+    res = centi_to_meter(187);
     printf("%.2lfm\n", res);
 
     return 0;
@@ -18,5 +18,5 @@ double centi_to_meter(double cm)
 {
     double meter;
     meter = cm / 100.0;
-    return meter;      
+    return meter;
 }
