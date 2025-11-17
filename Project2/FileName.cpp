2112c83ae4a7f61c3a90342a86c6987e630e3468
wjdsy <angel05020312@gmail.com>
2025-11-17 16:53:22 +0900

    wjdsy

---
+++ Project2/FileName.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void swap_pointer(int* pnA, int* pnB);
+void swap_normal(int nA, int nB);
+
+int main(void)
+{
+	int nA = 0, nB = 0;
+	nA = 10;
+	nB = 20;
+
+	int *pnA, *pnB;
+	pnA = &nA;
+	pnB = &nB;
+
+	swap_pointer(pnA, pnB);
+	//swap_normal(pnA, pnB);
+
+	printf("before swap:(nA,nB)=(%d, %d)\n", nA, nB);
+	return 0;
+}
+
+void swap_nomal(int nA, int nB)
+{
+	int nTemp = 0;
+	nTemp = nA;
+	nA = nB;
+	nB = nTemp;
+	printf("in the function:(nA,nB)=(%d, %d)\n", nA, nB);
+}
